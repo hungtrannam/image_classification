361ddd8d3bd68c104fee919ad8c0f1cf5258c5ad
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-04 09:10:38 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,37 +1,35 @@
-# =================================================================
 ''' Các hàm được định nghĩa trong gói chương trình
 1) Một số chương trình lệnh dùng chung cho các modules
 2) Link tải dữ liệu vào Python
 3) Kích thước font chữ và hệ màu
 4) Tạo folder lưu trữ hình ảnh trực quan (Graph) và kết quả (Output)
 '''
-# =================================================================
 
 #1
-import os											# (pip install os-sys)
+import os															# (pip install os-sys)
 
 import itertools	
 
 
-import pandas as pd		# Công cụ quản lý dữ liệu	# (pip install pandas)
-import numpy as np		# Công cụ đại số tuyến tính	# (pip install numpy)
-np.random.seed(3401)	# Tái lập kết quả
+import pandas as pd										# Công cụ quản lý dữ liệu	# (pip install pandas)
+import numpy as np										# Công cụ đại số tuyến tính	# (pip install numpy)
+np.random.seed(3401)										# Tái lập kết quả
 
 
-import arviz as az				# Đồ họa có posterior		# (pip install arviz)
-import matplotlib.pyplot as plt	# Đồ họa					# (pip install matplotlib)
-import seaborn as sns			# Đồ họa					# (pip install seaborn)
+import arviz as az										# Đồ họa có posterior		# (pip install arviz)
+import matplotlib.pyplot as plt													# (pip install matplotlib)
+import seaborn as sns														# (pip install seaborn)
 
 
-import statsmodels.api as sm	# Thống kê			# (pip install statsmodels)
-#import tensorflow as tf							# (pip install tensorflow)
-import probflow as pf								# (pip install probflow)
-#import tensorflow as tf							# (pip install tensorflow)
+import statsmodels.api as sm													# (pip install statsmodels)
+#import tensorflow as tf													# (pip install tensorflow)
+import probflow as pf														# (pip install probflow)
+#import tensorflow as tf													# (pip install tensorflow)
 
-import scipy.stats as ss		# Thống kê			# (pip install scipy)
+import scipy.stats as ss													# (pip install scipy)
 
 
-import cv2						# Trích suất, xử lý ảnh	# (pip install cv2)
+import cv2											# Trích suất, xử lý ảnh		# (pip install cv2)
 
 
 warning_status = "ignore"
@@ -47,15 +45,15 @@ test = 'D:\\A_NCKH_hung\\NCKH_Statistics\\Data\\test\\*.jpg'
 path = 'D:\\A_NCKH_hung\\NCKH_Statistics\\Modules\\Matlab\\Ex3_voihongngua\\*.jpg'
 
 #3
-plt.rcParams.update({'font.size': 15})	# Cỡ chữ
-bicolor = ['#e6154c', '#0e89ed']		# Hệ màu nhị phân
-fill = ['Greens','YlOrRd','Reds']		# Tạo 2 phổ màu liên tục/rời rạc cho 3 phân nhóm
+plt.rcParams.update({'font.size': 15})	
+bicolor = ['#e6154c', '#0e89ed']		
+fill = ['Greens','YlOrRd','Reds']		
 edge = ['#73cc06', '#fcba03', '#fc035a']
 
 #4
 cur_path = os.getcwd()
-viz_folder = os.path.join(cur_path, 'Graph')		# Tạo folder lưu hình ảnh trực quan
-output_folder = os.path.join(cur_path, 'Output')	# Tạo folder lưu các kết quả sơ khởi
+viz_folder = os.path.join(cur_path, 'Graph')		
+output_folder = os.path.join(cur_path, 'Output')	
 
 if os.path.isdir(viz_folder):
 	pass
