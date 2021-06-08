fd9ebb85490991b4cade0556d5b80bb6e3ab01f9
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-08 14:25:29 +0700

    Update data_import.py

--- Modules/data_import.py
+++ Modules/data_import.py
@@ -2,8 +2,7 @@
 ''' Các hàm được định nghĩa trong gói chương trình
 1) Tải dữ liệu từ đường link trong gói constants
 2) Tiền xử lý số liệu trong dataframe
-3) Chuyển dữ liệu theo phương pháp minmax hoặc chuẩn hóa
-4) Chia dữ liệu ban đầu thành hai tập training và testing với tỷ lệ thích hợp
+3) Chia dữ liệu ban đầu thành hai tập training và testing với tỷ lệ thích hợp
 '''
 
 
@@ -12,7 +11,6 @@ from constants import *	# Tải một số chương trình lệnh dùng chung
 from sklearn.model_selection import train_test_split	# Chia ngẫu nhiên dữ liệu
 from sklearn.experimental import enable_iterative_imputer	# Xử lý số liệu trống (NaN) bằng phương pháp MICE (Multivariate Imputation by Chained Equations)
 from sklearn.impute import IterativeImputer
-from sklearn.preprocessing import MaxAbsScaler	# Chuẩn hóa dữ liệu Minmax và/hoặc chuẩn hóa dữ liệu
 
 #1 
 def data_load(df_link: str):
@@ -39,17 +37,11 @@ def data_load(df_link: str):
 	print("Hoàn thành việc tải dữ liệu.")
 	return df, X, y
 
-#3
-def MaxAbsScaler_(X):
-	scaler = MaxAbsScaler()
-	xmat = scaler.fit_transform(X)
-	X = pd.DataFrame(xmat, columns = X.columns)
-	print("Hoàn thành việc chuẩn hóa MaxAbs dữ liệu")
-	return X
-
-# Tải dữ liệu thực tế
-df, X, y = data_load(df_link)
-
+#2
+def data_train_test(X, y, test_size):
+	X_train, Y_train, X_test, Y_test = train_test_split(X, y, test_size = test_size, random_state = 1)
+	print("Hoàn thành việc chia dữ liệu với tỷ lệ training {}% - {}%.".format((1-test_size)*100, test_size*100))
+	return X_train, Y_train, X_test, Y_test
 
 
 
