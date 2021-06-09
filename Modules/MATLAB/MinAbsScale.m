fba816aab5b39a648ce755002df19e230a555462
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-09 22:35:54 +0700

    Update MinAbsScale.m

--- Modules/MATLAB/MinAbsScale.m
+++ Modules/MATLAB/MinAbsScale.m
@@ -2,9 +2,9 @@ function MinAbs = MinAbsScale(Data)
     MinAbs = [];
     sz = size(Data);
     for i = 1:sz(1,2)
-        s(:,i) = Data(:,i);
-        d(i) = max(s(:,i));
-        MinAbs(:,i) = s(:,i)./d(i);
+        s(:, i) = Data(:, i);
+        d(i) = max(s(:, i));
+        MinAbs(:, i) = s(:, i)./d(i);
     end
     fprintf('Data has been stadarded.\n')
 end
