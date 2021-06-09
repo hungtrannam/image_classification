2e2ddb5520d44459fbc85d475bc76174d20638ee
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-09 21:03:04 +0700

    Update fit_SVM.py

--- Modules/fit_SVM.py
+++ Modules/fit_SVM.py
@@ -15,7 +15,7 @@ def fit_SVM_model(X, y):
     print('Khớp xong phân tích phân biệt tuyến tính Fisher với dữ liệu')
     return svm_model
 
-def class_report(X, y):
+def class_SVM_report(X, y):
   from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
   Logit_model = VC(C = 1e8, gamma = 0.0001, kernel = 'rbf', decision_function_shape = 'ovo').fit(X, y)
   cnf_matrix = confusion_matrix(y, Logit_model.predict(X))
