0919d3abbd6cbb0d81b43d5ce8e140a21268afe7
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-09 20:42:33 +0700

    Update data_description.py

--- Modules/data_description.py
+++ Modules/data_description.py
@@ -3,19 +3,21 @@ from data_import import *
 from scipy import stats
 from statsmodels.stats.descriptivestats import describe
 
-sum = describe(df)
-print(sum)
+
+def describe_(df):
+  summary = describe(df)
+  return summary
+
 
 sum1 = df.quantile(q = [0, 0.25, 0.5, 0.75, 1])
 print(sum1)
 
+def groupby_(val: str):
+  summary = df.groupby(val).describe().T
+  return summary
 
-sum2 = df.groupby('Y').describe().T
-print(sum2)
+#sns.pairplot(df, hue="Y")
+#fig_name = os.path.join(viz_folder, f"descr_glcm_data.pdf")
+#plt.savefig(fname = fig_name, format= "pdf")
 
-sns.pairplot(df, hue="Y")
-fig_name = os.path.join(viz_folder, f"descr_glcm_data.pdf")
-plt.savefig(fname = fig_name, format= "pdf")
-
-
-print(df.to_latex(index=False, caption = ('hello', 'heloo')))
+#print(df.to_latex(index=False, caption = ('hello', 'heloo')))
