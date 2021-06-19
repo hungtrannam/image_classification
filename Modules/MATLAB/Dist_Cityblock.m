6a213d1ff4dcc33136f5ff2f6527ea7edbd1ff65
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-19 16:41:02 +0700

    Update Dist_Cityblock.m

--- Modules/MATLAB/Dist_Cityblock.m
+++ Modules/MATLAB/Dist_Cityblock.m
@@ -1,3 +1,3 @@
 function D = Dist_Cityblock(a,b)
-   D = abs(a(1,1) - b(1,1)) + abs(a(1,2) - b(1,2)) + abs(a(1,3) - b(1,3)) + abs(a(1,4) - b(1,4));
+   D = abs(a(1,1) - b(1,1)) + abs(a(1,2) - b(1,2));
 end
