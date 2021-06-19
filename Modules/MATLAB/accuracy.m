d560a4ca63d2ab7d4d16da214ea30264ac5eafef
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-19 16:35:22 +0700

    Update and rename BayesErrorMethod.m to accuracy.m

--- Modules/MATLAB/accuracy.m
+++ Modules/MATLAB/accuracy.m
@@ -1,4 +1,4 @@
-function [BayesError, p1, p2] = BayesErrorMethod(bayes, traindata, testdata, f1, f2)
+function [acc, p1, p2] = accuracy(bayes, TrainData, TestData, f1, f2)
 % bayes: four options: {'BayesU' 'BayesL' 'BayesT' 'BayesC'}
 % train: Data has been standarded via Min-Max scale
 % testdata:
@@ -7,11 +7,11 @@ function [BayesError, p1, p2] = BayesErrorMethod(bayes, traindata, testdata, f1,
 
 p = [0; 0;];
 
-for i=1:length(traindata)
-    if traindata(i,end) == 1 
+for i=1:length(TrainData)
+    if TrainData(i,end) == 1 
         p(1,1) = p(1,1)+1;
     end
-    if traindata(i,end) == 2 
+    if TrainData(i,end) == 2 
         p(2,1) = p(2,1)+1;
     end
 end
@@ -19,14 +19,18 @@ end
 if bayes == 'BayesU'
     p1 = 1/2;
     p2 = 1/2;    
+    fprintf('Prior Probabilibty has been found via uniform dist.\n')
 elseif bayes == 'BayesL'
     p1 = (p(1,1)+1) / (sum(p)+2);
     p2 = (p(2,1)+1) / (sum(p)+2);    
+    fprintf('Prior Probabilibty has been found via Laplace method.\n')
 elseif bayes == 'BayesT'
     p1 = p(1,1) / sum(p);
     p2 = p(2,1) / sum(p);
+    fprintf('Prior Probabilibty has been found via trainning test.\n')
 elseif bayes == 'BayesC'
-    load('prior_FCM.mat');
+    [prior_C, p1,p2] = prior_FCM(TrainData);
+    fprintf('Prior Probabilibty has been found via FCM.\n')
 end
 
 g1 = (p1').*f1;
@@ -34,11 +38,12 @@ g2 = (p2').*f2;
 
 gtmin = min(g1,g2);
 
-BayesError = 1 - sum(gtmin)/length(testdata);
+fprintf('Accuracy:\n')
+acc = 1 - sum(gtmin)/length(TestData);
 
 if bayes == 'BayesC'
-    p1 = prior_FCM(1,1);
-    p2 = prior_FCM(2,1);
+    p1 = prior_C(1,1);
+    p2 = prior_C(2,1);
 else
     p1;
     p2;
