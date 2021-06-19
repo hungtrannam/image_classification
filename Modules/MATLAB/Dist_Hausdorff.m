03cb3d94e1b6b97b62e552dcd5e4d8bb9720c92c
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-19 16:42:19 +0700

    Update Dist_Hausdorff.m

--- Modules/MATLAB/Dist_Hausdorff.m
+++ Modules/MATLAB/Dist_Hausdorff.m
@@ -1,5 +1,5 @@
 function D = Dist_Hausdorff(a,b)
-   D = max(abs(a(1,1)-b(1,1)), abs(a(1,2)-b(1,2)));% + max(abs(a(1,3)-b(1,3)), abs(a(1,4)-b(1,4)));
+   D = max(abs(a(1, 1) - b(1, 1)), abs(a(1, 2) - b(1,2)));
 end
 
 
