2cbcffcd4f49b36d2681a4ea0c8e84f9d3046fa1
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-19 16:41:45 +0700

    Update Dist_Euclide.m

--- Modules/MATLAB/Dist_Euclide.m
+++ Modules/MATLAB/Dist_Euclide.m
@@ -1,3 +1,3 @@
-function V=Dist_Euclide(a,b)
-V=sqrt((a(1,1)-b(1,1))^2)+sqrt((a(1,2)-b(1,2))^2);%+sqrt((a(1,3)-b(1,3))^2)+sqrt((a(1,4)-b(1,4))^2);
+function D = Dist_Euclide(a, b)
+D = sqrt((a(1, 1) - b(1, 1))^2) + sqrt((a(1, 2) - b(1, 2))^2);
 end
