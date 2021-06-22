5267186b82f054557382b25b5dd792d35560d60a
Tran Nam Hung <68891580+hungtrannam@users.noreply.github.com>
2021-06-22 17:51:56 +0700

    Update data_import.py

--- Modules/Python/data_import.py
+++ Modules/Python/data_import.py
@@ -9,8 +9,6 @@
 from constants import *	# Tải một số chương trình lệnh dùng chung
 
 from sklearn.model_selection import train_test_split	# Chia ngẫu nhiên dữ liệu
-from sklearn.experimental import enable_iterative_imputer	# Xử lý số liệu trống (NaN) bằng phương pháp MICE (Multivariate Imputation by Chained Equations)
-from sklearn.impute import IterativeImputer
 
 #1 
 def data_load(df_link: str):
@@ -20,17 +18,15 @@ def data_load(df_link: str):
 	output: dataframes (dữ liệu gốc, dữ liệu biến giải thích và	dữ liệu biến phụ thuộc)
 	'''
 	# Tiến hành tải dữ liệu từ đường link trong gói 'constants'
-	df = pd.read_csv(df_link, sep = ',', decimal = '.')
+	df = pd.read_csv(df_link, 
+			 sep = ',', 
+			 decimal = '.')
 
 	# Nhân tố hóa biến định tính (category)
 	for col_name in df.columns:
 		if(df[col_name].dtype == 'object'):
 			df[col_name]= df[col_name].astype('category')
 			df[col_name] = df[col_name].cat.codes
-
-	# Xử lý các quan sát thiếu
-	imputer = IterativeImputer(sample_posterior=True).fit(df).transform(df)
-	df = pd.DataFrame(imputer, columns = df.columns)
 	
 	# Trích xuất dữ liệu thành biến tiên lượng (X) và biến giải thích (y)
 	X, y = df[df.columns[:-1]], df[df.columns[-1]]
@@ -39,7 +35,10 @@ def data_load(df_link: str):
 
 #2
 def data_train_test(X, y, test_size):
-	X_train, Y_train, X_test, Y_test = train_test_split(X, y, test_size = test_size, random_state = 1)
+	X_train, Y_train, X_test, Y_test = train_test_split(X, 
+							    y, 
+							    test_size = test_size, 
+							    random_state = 1)
 	print("Hoàn thành việc chia dữ liệu với tỷ lệ training {}% - {}%.".format((1-test_size)*100, test_size*100))
 	return X_train, Y_train, X_test, Y_test
 
